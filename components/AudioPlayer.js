7131965242342bbb4b41b10c133634971a359cef
Elton <eltonwhybrow@icloud.com>
2024-03-21 17:43:31 +0000

    test mouths

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, stagger } from 'framer-motion';
 
 const AudioPlayer = ({ src, post }) => {
     const [isPlaying, setIsPlaying] = useState(false);
@@ -12,36 +12,49 @@ const AudioPlayer = ({ src, post }) => {
         //     audioRef.current.pause();
         // } else {
         audioRef.current.play();
+        audioRef.current.onended = function () {
+            setIsPlaying(false);
+        };
         // }
         setIsPlaying(!isPlaying);
+
     };
 
     return (
         <>
             <motion.div
+                initial={{ scale: .9 }}
                 whileHover={{
                     scale: 1.2, // Scale up to 1.5 on hover
                     rotate: 360, // Rotate 360 degrees on hover
                     transition: { duration: .4 }, // Transition duration of 2 seconds
                 }}
+                stagger={0.9}
+
+
                 className='z-50'
             >
                 <motion.div
                     animate={{
                         scale: [1, 2, 2, 1, 1],
                         rotate: [0, 0, 270, 270, 0],
-                        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+                    }}
 
-                    }}>
+
+
+                >
                     <audio ref={audioRef} src={src} />
                     {/* <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button> */}
-                    <div className='py-2 mx-auto my-1 flex justify-center cursor z-50'>
+                    <div className=' relative py-2 mx-auto my-1 flex justify-center cursor z-50'>
                         <Image onClick={togglePlay} src={post.data.img} alt={post.data.title} width={200} height={230} className='h-full' />
+                        <div className={isPlaying ? `absolute ${post.data.title}-mouth move` : `absolute ${post.data.title}-mouth`}>
+                            <Image src={post.data.mouth} alt="mouth" width={94} height={66} className='h-full' />
+
+                        </div>
                     </div>
                 </motion.div>
             </motion.div>
         </>
     );
 };
-
 export default AudioPlayer;
